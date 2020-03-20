d64ebc5460cfbd89d871f60b4fadd95d325cbd22
Victoria Monteiro <52867523+Vmp309@users.noreply.github.com>
2020-03-19 23:32:05 -0300

    Create main.cpp

---
+++ 2aListadeExercícios/questao5/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#define MAX 50
+
+using namespace std;
+
+int main()
+{
+    fstream arquivo("arquivo.txt", ios::in | ios::out);
+    string nome;
+    string nome_lido;
+
+    ///Escrevendo qualquer coisa no arquivo.txt
+
+    if (!arquivo.is_open()){
+        cout << "arquivo.txt could not be opened!" << endl;
+    }else{
+        int opc = 0;
+        while (opc == 0){
+            cout << "Digite um nome: " << endl;
+            getline(cin, nome);
+
+            arquivo << nome << "\n";
+
+            cout << "Deseja digitar mais um nome?" << endl << "[0]Sim" << endl << "[1]Nao" << endl;
+            cin >> opc;
+            cin.ignore();
+        }
+    }
+
+
+   ///Lendo o arquivo.txt
+   vector <string> nomes[MAX];
+   int i = 0;
+    while (getline(arquivo, nome_lido)){
+        nomes[i].push_back(nome_lido);
+        i++;
+    }
+
+
+    int Numero_nomes_gravados = i + 1;
+    int contador = 0;
+
+
+   ///Gravando em copia.txt
+   fstream copia("copia.txt", ios::out);
+    if (!copia.is_open()){
+        cout << "copia.txt could not be opened!" << endl;
+    }else{
+        while (contador < Numero_nomes_gravados){
+            copia << nomes[contador] << "\n";
+            contador++;
+        }
+    }
+
+
+    return 0;
+}
